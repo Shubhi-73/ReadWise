ca8736f9d1fe8e3c5974481f0472b44d011b0e52
Sejal Shirude <130665640+seju13@users.noreply.github.com>
2023-10-27 22:17:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 
 import './App.css';
+import Axios from "axios";
 import Navbar from './Component/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import LoginSignUp from './Pages/LoginSignUp';
-function App() {
+import Compose from './Pages/Compose/Compose';
+import { useEffect, useState } from 'react';
+
+const App=()=>{
+  const [data,setData] =useState();
+
+  const getData=async()=>{
+    try {
+      const response = await Axios.get("http://localhost:3001/getData");
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+  useEffect(()=>{
+  getData() 
+},[]);
+
   return (
     <div>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<LoginSignUp />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/' element={<LoginSignUp />} />
+          <Route path='/Compose' element={<Compose />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+  }
 export default App;
