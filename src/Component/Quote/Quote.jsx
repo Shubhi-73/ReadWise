ededbd11f09a07b78e96e38f7e482c31389aab5c
VishakhaVan <130664733+VishakhaVan@users.noreply.github.com>
2023-10-28 15:45:27 +0530

    Add files via upload

--- src/Component/Quote/Quote.jsx
+++ src/Component/Quote/Quote.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import './Quote.css';
 
+function Quote() {
+    const quoteOfTheDay = "The only way to do great work is to love what you do. – Steve Jobs";
+
+    return (
+        <div className="quote-page">
+            <h2>Quote of the Day</h2>
+            <div className="quote">
+                <p>{quoteOfTheDay}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Quote;
