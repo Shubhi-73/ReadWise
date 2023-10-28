d4e1887dd4c0cc7cb35622d5266c53ae7ac86192
VishakhaVan <130664733+VishakhaVan@users.noreply.github.com>
2023-10-28 15:33:15 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Navbar from './Component/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import LoginSignUp from './Pages/LoginSignUp';
+import ComposePage from './Pages/Compose/Compose';
+import CollectionPage from './Pages/Collection/Collection';
+
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<LoginSignUp />} />
+          <Route path='/Compose' element={<ComposePage />} />
+          <Route path='/Collection' element={<CollectionPage />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
