67ec49a385259995bc81eb1d94c06bab143491aa
VishakhaVan <130664733+VishakhaVan@users.noreply.github.com>
2023-10-28 18:20:13 +0530

    Update Collection.jsx

--- src/Pages/Collection/Collection.jsx
+++ src/Pages/Collection/Collection.jsx
@@ -1,37 +1,15 @@
-import React, { useState } from 'react';
-import './Collection.css'; // Make sure to create a CSS file and import it
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Collection.css';
 
 function Collection() {
-    const [selectedTag, setSelectedTag] = useState('quotes');
-    const [isTagDropdownOpen, setIsTagDropdownOpen] = useState(false);
-
-    const handleTagChange = (tag) => {
-        setSelectedTag(tag);
-        setIsTagDropdownOpen(false);
-    };
-
     return (
-        <div className="collection-page">
-            <div className="buttons-container" onClick={() => handleTagChange('quotes')}>
-                All Quotes
-            </div>
-            <div className="tags-dropdown" onClick={() => setIsTagDropdownOpen(!isTagDropdownOpen)}>
-                Tags <i className={`fas fa-caret-${isTagDropdownOpen ? 'up' : 'down'}`}></i>
-                {isTagDropdownOpen && (
-                    <div className="tags-options">
-                        <button onClick={() => handleTagChange('quotes')}>Quotes</button>
-                        <button onClick={() => handleTagChange('memories')}>Memories</button>
-                        <button onClick={() => handleTagChange('songs')}>Songs</button>
-                    </div>
-                )}
+        <div className="home-page">
+            <h2>All Highlights</h2>
+            <Link to="/all-highlights">See all highlights</Link>
 
-            </div>
-            <div className="content">
-                {/* Content based on the selected tag */}
-                {selectedTag === 'quotes' && <div className="quotes-content">Quotes content here</div>}
-                {selectedTag === 'memories' && <div className="memories-content">Memories content here</div>}
-                {selectedTag === 'songs' && <div className="songs-content">Songs content here</div>}
-            </div>
+            <h2>Tags</h2>
+            <Link to="/tags">See by tags</Link>
         </div>
     );
 }
