8f42bbd18e4854e234d125b4008b7209e1fc518d
VishakhaVan <130664733+VishakhaVan@users.noreply.github.com>
2023-10-28 15:25:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,28 @@
 
 import './App.css';
-import Axios from "axios";
 import Navbar from './Component/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import LoginSignUp from './Pages/LoginSignUp';
 import Compose from './Pages/Compose/Compose';
 import { useEffect, useState } from 'react';
+import CollectionPage from './Pages/Collection/Collection';
 
-const App=()=>{
-  const [data,setData] =useState();
-
-  const getData=async()=>{
-    try {
-      const response = await Axios.get("http://localhost:3001/getData");
-      setData(response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }
-  useEffect(()=>{
-  getData() 
-},[]);
+function App() {
 
   return (
     <div>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/Home' element={<Home />} />
-          <Route path='/' element={<LoginSignUp />} />
-          <Route path='/Compose' element={<Compose />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<LoginSignUp />} />
+          <Route path='/Compose' element={<ComposePage />} />
+          <Route path='/Collection' element={<CollectionPage />} />
+
         </Routes>
       </BrowserRouter>
     </div>
   );
-  }
+}
 export default App;
