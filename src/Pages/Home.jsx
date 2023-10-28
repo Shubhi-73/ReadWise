e0bc4e699d32b89401b360ccf9d6dbb331202210
VishakhaVan <130664733+VishakhaVan@users.noreply.github.com>
2023-10-28 15:34:32 +0530

    Create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Quote from '../Component/Quote/Quote'
+
+const Home = () => {
+    return (
+        <div>
+            <Quote />
+        </div>
+    )
+}
+export default Home
