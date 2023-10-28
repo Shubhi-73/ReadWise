02a10c699f1c6c11aa14d1b6f71c0af10ac947a7
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2023-10-28 23:52:36 +0530

    Update AllHighlights.jsx
    Now iterating through all the documents passed

--- src/Pages/Collection/AllHighlights.jsx
+++ src/Pages/Collection/AllHighlights.jsx
@@ -1,9 +1,9 @@
 // components/AllHighlightsPage.js
-import React from 'react';
 import { Link } from 'react-router-dom';
-import './AllHighlightsPage.css';
+import React, { useState, useEffect } from "react";
+import './Collection.css';
 
-function AllHighlights() {
+function Collection() {
     // Example list of quotes
     const quotes = [
         { title: 'Quote 1', description: 'Description for Quote 1' },
@@ -11,21 +11,47 @@ function AllHighlights() {
         // Add more quotes here
     ];
 
+    const [data, setData] = useState("");
+
+
+
+    useEffect(() => {
+      fetch("http://localhost:8000/Collection")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error:', error));
+    }, []);
+
     return (
         <div className="all-highlights-page">
-            <h2>All Highlights</h2>
-            <ul className="quote-list">
-                {quotes.map((quote, index) => (
-                    <li key={index}>
-                        <Link to={`/quote/${index}`}>
-                            <h3>{quote.title}</h3>
-                            <p>{quote.description}</p>
-                        </Link>
+
+          <h1>MongoDB Documents</h1>
+          <ul>
+            {data.length === 0 ? (
+              <p>No data available.</p>
+            ) : (
+              // Use a for loop to iterate through the data
+              (() => {
+                const items = [];
+                for (let i = 0; i < data.length; i++) {
+                  const document = data[i];
+                  items.push(
+                    <li key={document._id}>
+                      {/* Render the content of each MongoDB document */}
+                      <p>Book: {document.book}</p>
+                      <p>Content: {document.content}</p>
+                      {/* Add more properties as needed */}
                     </li>
-                ))}
-            </ul>
+                  );
+                }
+                return items;
+              })()
+            )}
+          </ul>
+
         </div>
     );
 }
 
-export default AllHighlights;
+
+export default Collection;
