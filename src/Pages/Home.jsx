bd0ef2326ad94949d0f7ed0c0d44f9f5c7e87765
VishakhaVan <130664733+VishakhaVan@users.noreply.github.com>
2023-10-30 21:45:40 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Quote from '../Component/Quote/Quote'
+import Navbar from '../Component/Navbar/Navbar'
 
 const Home = () => {
     return (
         <div>
+             <Navbar />
             <Quote />
         </div>
     )
