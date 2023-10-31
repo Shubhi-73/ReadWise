4892e856fc12948f6eb7ad5aca6e436ee94738c4
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2023-10-31 17:03:07 +0530

    Update Home.jsx
    Displays quote of the day

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Quote from '../Component/Quote/Quote'
-import Navbar from '../Component/Navbar/Navbar'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './Navbar';
+
 
 const Home = () => {
+  //
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/home")
+    .then((response) => response.json())
+    .then((data) => {
+        setContent(data.content);
+        setTitle(data.book);
+      })
+    .catch((error) => console.error('Error:', error));
+  }, []);
+
+
+
     return (
+
         <div>
-             <Navbar />
-            <Quote />
+          <Navbar />
+
+        <p>{title}</p>
+        <p>{content}</p>
         </div>
     )
 }
