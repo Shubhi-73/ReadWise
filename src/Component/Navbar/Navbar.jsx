a366d10e53f10e7051154ed50b40694df8ecef25
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2023-11-03 22:37:42 +0530

    Update Navbar.jsx

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,24 +1,53 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { Link } from 'react-router-dom';
-const Navbar = () => {
-    const [menu, setMenu] = useState("Home");
-    return (
-        <div className='navbar'>
-            <div className='nav-logo'>
-                <p>QuoteVault</p>
-            </div>
-            <ul className='nav-menu'>
-                <li onClick={() => { setMenu("Home") }}><Link style={{ textDecoration: 'none' }} to='/Home'>Home</Link>{menu === "Home" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("Compose") }}><Link style={{ textDecoration: 'none' }} to='/Compose'>Compose</Link>{menu === "Compose" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("Collection") }}><Link style={{ textDecoration: 'none' }} to='/Collection'>Collection</Link>{menu === "Collection" ? <hr /> : <></>}</li>
-            </ul>
-            <div className='lb'>
-                <Link to='/'><button>Login</button></Link>
-            </div>
-
-        </div>
-    )
+/* Style for the main navigation container */
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #A6B1E1;
+  padding: 10px 20px;
 }
 
-export default Navbar
+/* Style for the logo on the left */
+.nav-logo p {
+  font-size: 36px;
+  font-weight: bold;
+  color: #424874;
+  margin: 0;
+  background-color: #F4EEFF;
+  padding: 10px 20px;
+  border-radius: 4px;
+}
+
+/* Style for the navigation menu on the right */
+.nav-menu {
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  justify-content: flex-end; /* Align to the right */
+}
+
+/* Style for each menu item */
+.nav-menu li {
+  margin-right: 20px;
+  position: relative;
+}
+
+/* Style for menu item links */
+.nav-menu a {
+  text-decoration: none;
+  font-size: 18px;
+  color: #424874;
+}
+
+/* Style for the horizontal line under the active menu item */
+.nav-menu li hr {
+  border: none;
+  border-top: 2px solid #ACD8AA;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
