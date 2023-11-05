414412a84778baf5126d7c63c9e67f17a12f9884
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2023-11-05 09:56:21 +0530

    Update Collection.jsx
    fixed issues with the false statement and set the default value of setTag.

--- src/Pages/Collection/Collection.jsx
+++ src/Pages/Collection/Collection.jsx
@@ -1,41 +1,69 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Navbar from './Pages/Navbar';
-import Login from './Pages/Login';
-import Home from './Pages/Home';
-import Collection from './Pages/Collection';
-import Compose from './Pages/Compose';
+import Navbar from './Navbar';
 
-function App() {
-  const [message, setMessage] = useState("");
+import './Collection.css';
 
+function Collection() {
+
+  const [data, setData] = useState("");
+  const [selectedTag, setSelectedTag] = useState("quote");
 
   useEffect(() => {
-    fetch("http://localhost:8000")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
+    fetch("http://localhost:8000/Collection")
+    .then((response) => response.json())
+    .then((data) => setData(data))
+    .catch((error) => console.error('Error:', error));
   }, []);
 
-  return (
-    <div>
-    <div className="App">
-    <BrowserRouter>
-    
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/Compose' element={<Compose />} />
-        <Route path='/Collection' element={<Collection />} />
-        <Route path='/' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
 
+    return (
+      <div>
+          <Navbar />
 
-      <h1>Hi! {message}</h1>
-    </div>
-  );
+        <h2>Filter according to tag</h2>
+        {/*<input type="text" id="tagValue" value="quote"></input>*/}
+        <select id="tagValue" onChange={(e) => setSelectedTag(e.target.value)}>
+        {/* Add options for tag filtering */}
+        <option value="quote">Quote</option>
+        <option value="song">Song</option>
+        <option value="entry">Entry</option>
+      </select>
+
+        <ul>
+          {data.length === 0 ? (
+            <p>No data available.</p>
+          ) : (
+            // Use a for loop to iterate through the data
+            (() => {
+              const items = [];
+              for (let i = 0; i < data.length; i++) {
+                const note = data[i];
+                items.push(
+                    /* Render the content of each MongoDB document */
+                    selectedTag === note.tag ? (
+                      <li key={note._id}>
+                    <div>
+                  <p className = "heading">{note.book}</p>
+                  <p className = "content">{note.content}</p>
+                {/*  <p>Tag: {note.tag}</p> */}
+                </div>
+                </li>
+      ):(null)
+                /* Add more properties as needed */
+
+                );
+              }
+              return items;
+            })()
+          )}
+        </ul>
+
+      </div>
+    );
 }
 
-export default App
+export default Collection;
