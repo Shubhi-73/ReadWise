4696c7c14cdda326082ad8636d9fce10a6d0c519
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2023-11-05 19:12:42 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import './Home.css';
 import Navbar from './Navbar';
 
 
@@ -8,6 +8,7 @@ const Home = () => {
   //
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
+  const [user, setUser] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8000/home")
@@ -15,20 +16,19 @@ const Home = () => {
     .then((data) => {
         setContent(data.content);
         setTitle(data.book);
+        setUser(data.message);
       })
     .catch((error) => console.error('Error:', error));
   }, []);
 
-
-
-    return (
-
-        <div>
-          <Navbar />
-
-        <p>{title}</p>
-        <p>{content}</p>
-        </div>
-    )
+  return (
+<div className="Home">
+  <Navbar />
+  <h1>Welcome, {user}!</h1>
+  <p>{title}</p>
+  <p>{content}</p>
+  <p className="special-greeting">Explore Your Quotes</p>
+</div>
+);
 }
 export default Home
