e94ba6fb0a7daf3f5c89dc13333eef5b7dc32724
Sejal Shirude <130665640+seju13@users.noreply.github.com>
2023-11-08 14:17:15 +0530

    Update Collection.jsx

--- src/Pages/Collection/Collection.jsx
+++ src/Pages/Collection/Collection.jsx
@@ -2,29 +2,58 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Navbar from './Navbar';
+import Navbar from "../../Component/Navbar/Navbar";
+import Footer from "../../Component/Footer/Footer";
 
 import './Collection.css';
 
 function Collection() {
 
-  const [data, setData] = useState("");
+ // const [data, setData] = useState("");
   const [selectedTag, setSelectedTag] = useState("quote");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/Collection")
-    .then((response) => response.json())
-    .then((data) => setData(data))
-    .catch((error) => console.error('Error:', error));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/Collection")
+  //   .then((response) => response.json())
+  //   .then((data) => setData(data))
+  //   .catch((error) => console.error('Error:', error));
+  // }, []);
+
+  const data = [
+    {
+      _id: 1,
+      book: "Book 1",
+      content: "Content 1",
+      tag: "quote",
+    },
+    {
+      _id: 4,
+      book: "Book 4",
+      content: "Content 4",
+      tag: "quote",
+    },
+    {
+      _id: 2,
+      book: "Book 2",
+      content: "Content 2",
+      tag: "song",
+    },
+    {
+      _id: 3,
+      book: "Book 3",
+      content: "Content 3",
+      tag: "entry",
+    },
+    // Add more objects as needed
+  ];
 
 
     return (
-      <div>
+      <div className='collectionPage'>
           <Navbar />
+          <div className='collection'>
 
-        <h2>Filter according to tag</h2>
+        <p>Filter according to tag
         {/*<input type="text" id="tagValue" value="quote"></input>*/}
         <select id="tagValue" onChange={(e) => setSelectedTag(e.target.value)}>
         {/* Add options for tag filtering */}
@@ -32,8 +61,9 @@ function Collection() {
         <option value="song">Song</option>
         <option value="entry">Entry</option>
       </select>
+      </p>
 
-        <ul>
+        <ul id="ul">
           {data.length === 0 ? (
             <p>No data available.</p>
           ) : (
@@ -45,7 +75,7 @@ function Collection() {
                 items.push(
                     /* Render the content of each MongoDB document */
                     selectedTag === note.tag ? (
-                      <li key={note._id}>
+                      <li id='li' key={note._id}>
                     <div>
                   <p className = "heading">{note.book}</p>
                   <p className = "content">{note.content}</p>
@@ -62,6 +92,8 @@ function Collection() {
           )}
         </ul>
 
+      </div>
+      <Footer />
       </div>
     );
 }
