2f851e258fedd584acdf2be4959eec76051a24ec
Sejal Shirude <130665640+seju13@users.noreply.github.com>
2023-11-08 13:26:38 +0530

    Update Navbar.jsx

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from 'react'
 import './Navbar.css'
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
-     const [menu, setMenu] = useState("Home");
-    return (
+    const[menu,setMenu]=  useState("Home");
+  return (
+    <body>
+              <div className='navbar'>
+            <div className='nav-logo'>
+                <p>QuoteVault</p>
+            </div>
+            <ul className='nav-menu'>
+                <li onClick={() => { setMenu("Home") }}><Link style={{ textDecoration: 'none' }} to='/Home'>Home</Link>{menu === "Home"}</li>
+                <li onClick={() => { setMenu("Compose") }}><Link style={{ textDecoration: 'none' }} to='/Compose'>Compose</Link>{menu === "Compose"}</li>
+                <li onClick={() => { setMenu("Collection") }}><Link style={{ textDecoration: 'none' }} to='/Collection'>Collection</Link>{menu === "Collection"}</li>
+                <li onClick={() => { setMenu("Faq") }}><Link style={{ textDecoration: 'none' }} to='/Faq'>FAQs</Link>{menu === "Faq"}</li>
+            </ul>
+            <div className='lb'>
+                <Link to='/'><button>Login</button></Link>
+            </div>
 
-<div classname="navbar">
-<div className="nav-logo">
-  <p>QuoteVault</p>
-</div>
-<ul className="nav-menu">
-  <li onClick={() => { setMenu("Home") }}><Link style={{ textDecoration: 'none' }} to='/Home'>Home</Link>
-    {menu === "Home" ? <hr /> : <></>}
-  </li>
-  <li onClick={() => { setMenu("Compose") }}><Link style={{ textDecoration: 'none' }} to='/Compose'>Compose</Link>
-    {menu === "Compose" ? <hr /> : <></>}
-  </li>
-  <li onClick={() => { setMenu("Collection") }}><Link style={{ textDecoration: 'none' }} to='/Collection'>Collection</Link>
-    {menu === "Collection" ? <hr /> : <></>}
-  </li>
-  </ul>
-</div>
-);
+        </div>
+        </body>
+    
+  )
 }
-
 
 export default Navbar
