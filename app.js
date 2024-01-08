7d309331be57d4fd5a65cb47c9225ee6e844e6fb
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2024-01-08 10:48:47 +0530

    Update app.js with server.js code

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
 const app = express();
-const nodemailer = require("nodemailer")
-const {google} = require("googleapis")
-const JSONStream = require('JSONStream');
 
-//------------------------for NYtimes API------------
-const https = require("https");
+/////database connection///
+const mongoose = require("mongoose");
 
-//----------------google api----------
-const CLIENT_ID = "226419603487-b7cp7tgrffkiqbv1i288m2sge8666qt9.apps.googleusercontent.com"
-const CLIENT_SECRET= "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"
-const REDIRECT_URI = "https://developers.google.com/oauthplayground"
-const REFRESH_TOKEN = "1//04msO8HSV3P5DCgYIARAAGAQSNwF-L9IrznHDq1Df_D4qG4Pp4K7hL43wR86RSkgJS5uLPcQ2cyLWu7QSTK2vV6HILZQtkn-r8s0"
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-    //--------------google api end---------------
-app.set('view engine', 'ejs');
-
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
-
-//------------------------------------------------------------- mongoose stuff
-
-mongoose.connect("mongodb://localhost:27017/ReadwiseDB", {
+mongoose.connect("mongodb+srv://srivastavasnigdha519:mjQnzizxDENnZQ8G@readone.ewtbxdf.mongodb.net/ReadwiseDB?retryWrites=true&w=majority", {
   useNewUrlParser: true
 });
-//--------------------------------------------------------------
 
 const userSchema = {
   email: String,
@@ -44,258 +14,181 @@ const userSchema = {
   password: String
 }
 
-
 const noteSchema = {
   user: String,
-  index: Number,
   book: String,
-  content: String
+  content: String,
+  tag: String
 };
 
+const dailyQuoteSchema = {
+  user: String,
+  date: String,
+  book: String,
+  content: String,
+  tag: String
+};
+
+const Quote = mongoose.model("Quote", dailyQuoteSchema); //singular version of the collection
 const Note = mongoose.model("Note", noteSchema); //singular version of the collection
 const User = mongoose.model("User", userSchema); //singular version of the collection
-//------------------------------------------------------------------
+
+app.use(express.json());
+let userName = "user1";
+let foundNote;
+let todayQuote;
+let list;
+let tags;
 
 
-
-//------------------------------------------
-//collect username and filter notes accordingly
-let userName = "me";
-let passWord;
-let numberOfPosts;
-let found_note = new Note();
-//get function stuff--------------------
-
-let titleofBook,authorofBook;
-app.get("/", function(req,res){
-res.redirect("login");
-});
-
-app.get("/home", function(req,res){
-///////////////////////////////////////////
-
-
-
-
-////////////////////////////
-
-Note.countDocuments({user: userName}, function(err, count)
+app.get('/home', async (req, res) => 
 {
+  try{
+      let currentDate = new Date().toJSON().slice(0, 10);
+      let passContent;
+      let passBook;
+      const todayQuote = await Quote.find({user: userName, date: currentDate}); //find the mailID of the user
+    //in the event that the daily mail code didn't run
+      if (todayQuote.length === 0) {
+          console.log("length is 0");
+          passContent = "Nothing is as good or bad as it seems - NYU prof. Scott Galloway";
+          passBook = "Psychology of Money";
 
-  if(count===0){
-    const note1 = new Note({
-      user: userName,
-      index: 1,
-      book: "Think Again",
-      content: "Freeze and seize"
-    });
+        } else {
 
-    note1.save();
-
-    const note2 = new Note({
-      user: userName,
-      index: 2,
-      book: "Psychology of Money",
-      content: "Nothing is as good or bad as it seems - NYU prof. Scott Galloway"
-    });
-
-    note2.save();
-
-    const note3 = new Note({
-      user: userName,
-      index: 3,
-      book: "Mindset",
-      content: "In one world, effort is a bad thing. It, like failure, means you’re not smart or talented. If you were, you wouldn’t need effort. In the other world, effort is what makes you smart or talented."
-    });
-
-    note3.save();
-
-    count = 3;
-  }
-
-  const pipeline = [
-      { $match: {user:userName} },
-      { $sample: { size: 1 } },
-    ];
-
-Note.aggregate(pipeline, function(err, foundNote)
-     {
-      console.log("this is the found note: ");
-      console.log(foundNote);
-      found_note = foundNote[0];
-      if (!err)
-      {
-        //where home render was here
-        //sending a mail through API
-        async function sendMail()
-        {
-          try
-          {
-              const accessToken = await oAuth2Client.getAccessToken();
-
-              const transport = nodemailer.createTransport(
-              {
-                  service: 'gmail',
-                  auth:
-                  {
-                      type: 'OAuth2',
-                      user: 'srivastava.snigdha519@gmail.com',
-                      clientId: CLIENT_ID,
-                      clientSecret: CLIENT_SECRET,
-                      refreshToken: REFRESH_TOKEN,
-                      accessToken: accessToken,
-                  },
-              });
-
-              const mailOptions =
-              {
-                from: 'QuoteVault <srivastava.snigdha519@gmail.com>',
-                to: ['srivastava.snigdha519@gmail.com'],
-                subject: 'Quote of the day!',
-                text: 'Hello from the other side',
-                html: '<div style="background-color: #95D1CC;text-align: center;padding: 1em; border-radius: 25% 10%; font: Roboto;">'+'<h2>'+foundNote[0].book+'</h2><h3 style="font-style: italic;">'+foundNote[0].content+'</h3></div>'
-              }
-
-              const result = await transport.sendMail(mailOptions);
-              return result;
-          } catch (error)
-          {
-            return error;
-          }
+    passContent = todayQuote[0].content;
+    passBook = todayQuote[0].book;
+    //res.json(todayQuote[0]);
         }
-
-        sendMail()
-        .then((result) => console.log('Email sent...', result))
-        .catch((error) => console.log(error.message));
-
-      }
-  });
-});
-
-
-
-
-/////////////////////////////////
-
-      res.render('home', {
-        book: found_note.book,
-        content: found_note.content,
-        user: userName
-      });
-});//get
-
-  /////////////////////////////
-
-
-
-
-app.get("/display", function(req,res){
-  Note.find({user: userName}, function(err, foundPosts){
-    if(!err){
-      res.render("display", {newPost: foundPosts});
-    }
-  });
-});
-
-
-app.get("/compose", function(req,res){
-  res.render("compose")
-});
-
-//for new users
-app.get("/register", function(req,res){
-  res.render("register")
-});
-
-//for existing users
-app.get("/login", function(req,res){
-  res.render("login")
-});
-
-app.get("/loginError", function(req,res){
-  res.render("loginError")
-});
-
-
-
-app.post("/login", function(req,res){
-
-userName = req.body.emailInput;
-password = req.body.passwordInput;
-
-User.findOne({email: userName}, function(err, foundUser){
-
-if(err){
-  console.log("error at finding user"+err);
-}
-else{
-  if(foundUser){
-    if(foundUser.password === password){
-      console.log("PASSWORD MATCHED")
-      res.redirect("/home");
-    }
-    else{
-      console.log("INCORRECT PASSWORD")
-      res.redirect("loginError");
-    }
+        res.json({content: passContent ,book: passBook ,message: userName});
   }
-  else{ //incorrect email
-    res.redirect("loginError");
+  catch(err){
+    console.error('Error fetching note:', err);
+    res.status(500).json({ error: 'Failed to retrieve user note' });
   }
+});
+
+
+
+app.get('/Collection', async (req, res) => {
+  try{
+    const list = await Note.find({user: userName});
+    res.json(list);
+    console.log(list);
+  }
+  catch(err){
+    console.error('Error fetching notes:', err);
+
+    res.status(500).json({ error: 'Failed to retrieve notes' });
+  }
+});
+
+
+app.post('/deleteData', async(req, res) => {
+try{
+  const idOfNoteToBeDeleted = req.body;
+  const check = await Note.deleteOne({user: userName, _id: idOfNoteToBeDeleted.noteId});
+  console.log("Successfully deleted");
+  res.status(200).json({message: "deleted successfully"});
 }
-
-
+catch(err){
+  console.error('Error fetching notes:', err);
+  res.status(500).json({ error: 'Failed to retrieve notes' });
+}
 });
 
 
-});
-
-
-app.post("/register", function(req,res){
-
-  const newUser = new User({
-    email: req.body.emailInput,
-    password: req.body.passwordInput,
-    emailId: req.body.emailIdInput
-  });
-
-userName = req.body.emailInput;
-  newUser.save(function(err){
-
-    if(err){
-      console.log("error in register")
-    }
-    else{
-
-      //initialize new collection
-
-
-
-        res.redirect("/");
-    }
-  })
-
-
-});
-
-app.post("/home", function(req,res)
-{
-
+app.post('/sendComposeData', async(req, res) => {
   const newNote = new Note(
     {
       user: userName,
-      index: numberOfPosts + 1,
       book: req.body.title,
-      content: req.body.highlight
+      content: req.body.description,
+      tag: req.body.tag.toLowerCase()
     } );
     console.log(newNote);
-  numberOfPosts = numberOfPosts + 1;
   newNote.save();
-  res.redirect("/display");
+res.status(200).json({message: "saved successfully"});
 });
 
-console.log("the end")
-app.listen(3000, function()
-{
-  console.log("Server started on port 3000");
+
+app.post('/sendLoginData', async(req, res) => {
+try{
+userName = req.body.username;
+password = req.body.password;
+
+  const foundUser = await User.findOne({email: userName});
+
+    if(foundUser){
+      if(foundUser.password === password){
+        console.log("PASSWORD MATCHED")
+        res.status(200).json({message: "clear"});
+      }
+      else{
+        console.log("INCORRECT PASSWORD");
+        alert("Invalid login");
+      res.status(400).json({message: "incorrect credentials"});
+      }
+    }
+    else{ //incorrect email
+      console.log("INCORRECT EMAIL")
+    }
+  }
+
+  catch(err){
+    console.error('Error fetching notes:', err);
+    res.status(500).json({ error: 'Failed to retrieve notes' });
+  }
+
+});
+
+app.post('/sendSignUpData', async(req, res) => {
+userName = req.body.username;
+console.log("reached in sign up post")
+const newUser = new User(
+  {
+    email: req.body.username,
+    emailId: req.body.email,
+    password: req.body.password
+  } );
+
+  console.log(newUser);
+newUser.save();
+
+//setting up quotes for the new user
+
+const note1 = new Note({
+  user: userName,
+  tag: "quote",
+  book: "Think Again",
+  content: "Freeze and seize"
+});
+
+note1.save();
+
+const note2 = new Note({
+  user: userName,
+  tag: "quote",
+  book: "Psychology of Money",
+  content: "Nothing is as good or bad as it seems - NYU prof. Scott Galloway"
+});
+
+note2.save();
+
+const note3 = new Note({
+  user: userName,
+  tag: "quote",
+  book: "Mindset",
+  content: "In one world, effort is a bad thing. It, like failure, means you’re not smart or talented. If you were, you wouldn’t need effort. In the other world, effort is what makes you smart or talented."
+});
+
+note3.save();
+//check if user with the same username exists
+ res.status(200).json({message: "Success"})
+
+});
+
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000.`);
 });
